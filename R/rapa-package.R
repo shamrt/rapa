1679b93bb3a244c414ea2b1c0d29fcf03aaef276
Shane Martin <dev.sh@nemart.in>
2015-04-17 10:34:03 -0400

    Add package documentation

---
+++ R/rapa-package.R
@@ -0,0 +1,10 @@
+#' Generate APA 6-styled output from R objects.
+#'
+#' \code{rapa} allows you to dynamically inject R objects into your manuscript and does
+#' its best to style them in APA 6 format.
+#'
+#' A series of helper functions that return APA-styled text in \href{http://www.latex-project.org}{LaTeX} or \href{http://pandoc.org/index.html}{pandoc-style markdown} format. Especially handy when used with \href{http://rmarkdown.rstudio.com}{RMarkdown}.
+#'
+#' @docType package
+#' @name rapa-package
+NULL
